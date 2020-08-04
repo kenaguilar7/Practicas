39c9d76c4feedcf6ff55e2127c79d0e6daea9ed0
Kenneth Aguilar <kenaguilar.steve@gmail.com>
2020-08-03 20:26:27 -0600

    Entity framework project

---
+++ Entity Framework/DemoWebApp/EFDemoApp/EFDemoWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFDataAccessLibrary.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace EFDemoWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
+
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
